05e8805e90bf6176b60f7c752bc601c75e1bc8c2
Aman Pareek <107720919+Amanp30@users.noreply.github.com>
2022-08-24 17:28:12 +0530

    Add files via upload

---
+++ middlewares/fileupload.js
@@ -0,0 +1,13 @@
+const multer = require("multer");
+
+// Storage Engin That Tells/Configures Multer for where (destination) and how (filename) to save/upload our files
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./images"); //important this is a direct path fron our current file to storage location
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "--" + file.originalname);
+  },
+});
+
+exports.upload = multer({ storage: fileStorageEngine });
