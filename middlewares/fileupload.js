968fd262276271a2e858fddd14d2236779bc6731
Aman Pareek <107720919+Amanp30@users.noreply.github.com>
2022-08-24 17:51:07 +0530

    Update fileupload.js

--- middlewares/fileupload.js
+++ middlewares/fileupload.js
@@ -1,4 +1,11 @@
 const multer = require("multer");
+const fs = require("fs")
+
+if (!fs.existsSync('./images')) {
+    fs.mkdir("./images", function (e) {
+        console.log('Directory not found but dir created.');
+    });
+}
 
 // Storage Engin That Tells/Configures Multer for where (destination) and how (filename) to save/upload our files
 const fileStorageEngine = multer.diskStorage({
@@ -6,7 +13,7 @@ const fileStorageEngine = multer.diskStorage({
     cb(null, "./images"); //important this is a direct path fron our current file to storage location
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "--" + file.originalname);
+    cb(null, "user profile --" + file.originalname);
   },
 });
 
